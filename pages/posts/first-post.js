515cf7ba83d2d638f92ff2e7d288a9c26370f55c
yunj <yunj8649@xingxingmobility.com>
2021-11-28 16:40:25 +0900

    Session) Navigate Between Pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <Fragment>
+             <h1>First Post</h1>
+
+            <Link href="/posts/two-post">
+                <a>click router two-post page</a>
+            </Link>
+        </Fragment>
+    )
 }
--- pages/posts/two-post.js
+++ pages/posts/two-post.js
@@ -1,3 +1,15 @@
+import { Fragment } from "react";
+import Link from 'next/link';
+
 export default function TwoPost() {
-    return <h1>Tow Post</h1>
+    return (
+        <Fragment>
+            <h1>Tow Post</h1> 
+
+            Read{' '}
+            <Link href="/posts/first-post">
+                <a>this page!</a>
+            </Link>
+        </Fragment>
+    )
 }
